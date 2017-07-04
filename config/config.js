d855cda063c70f12dbe5ec00d689e4766b9363a3
Vladimir <vlad@simplytechnologies.net>
2017-07-04 18:44:22 +0400

    deploy

--- config/config.js
+++ config/config.js
@@ -20,7 +20,7 @@ module.exports = {
     },
 
     production: {
-        db: 'mongodb://heroku/weekschedule',
+        db: 'mongodb://heroku_896l663q:38e270fls3iobpmf7p9iok5tt5@ds149382.mlab.com:49382/heroku_896l663q',
         gapiClientId: '735955037545-r8ujuf1njsm3sv02t371npmmj6ieelaa.apps.googleusercontent.com',
         requireMail: 'simplytechnologies.net'
     }
